912c9b1150a0b1c52f365f13a73fc06715444755
koreansandwich <jdh4673@ajou.ac.kr>
2024-09-11 18:14:21 +0900

    my page

--- lib/screens/my_page_screen.dart
+++ lib/screens/my_page_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 class MyPageScreen extends StatefulWidget {
   @override
@@ -8,6 +12,24 @@ class MyPageScreen extends StatefulWidget {
 
 class _MyPageScreenState extends State<MyPageScreen> {
   User? user = FirebaseAuth.instance.currentUser;
+  String? profileImageUrl;
+  String? displayName;
+  List<String> favoriteGenres = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData(); // Firestore에서 사용자 정보 불러오기
+  }
+
+  Future<void> _loadUserData() async {
+    DocumentSnapshot userData = await FirebaseFirestore.instance.collection('users').doc(user!.uid).get();
+    setState(() {
+      profileImageUrl = userData['profileImageUrl'];
+      displayName = userData['name'];
+      favoriteGenres = List<String>.from(userData['favoriteGenres']);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,30 +41,41 @@ class _MyPageScreenState extends State<MyPageScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Text('User Name: ${user?.displayName ?? 'No Name'}'),
-            Text('Email: ${user?.email ?? 'No Email'}'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.of(context).pop();
-              },
-              child: Text('Logout'),
+            // 프로필 사진
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: profileImageUrl != null
+                  ? NetworkImage(profileImageUrl!)
+                  : AssetImage('assets/default_profile.png') as ImageProvider,
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EditProfileScreen()),
-                ).then((_) async {
-                  // 프로필 수정 후 사용자 정보를 다시 로드
-                  await FirebaseAuth.instance.currentUser?.reload();
-                  setState(() {
-                    user = FirebaseAuth.instance.currentUser;
-                  });
-                });
-              },
-              child: Text('Edit Profile'),
+            SizedBox(height: 10),
+            // 유저 이름과 수정 버튼
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(displayName ?? 'No Name', style: TextStyle(fontSize: 18)),
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EditProfileScreen()),
+                    ).then((_) => _loadUserData()); // 프로필 수정 후 데이터 다시 로드
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            // 좋아하는 영화 장르
+            Text('My favorite Movie', style: TextStyle(fontSize: 16)),
+            Wrap(
+              spacing: 8.0,
+              children: favoriteGenres.map((genre) {
+                return Chip(
+                  label: Text(genre),
+                );
+              }).toList(),
             ),
           ],
         ),
@@ -58,18 +91,68 @@ class EditProfileScreen extends StatefulWidget {
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
   final TextEditingController nameController = TextEditingController();
+  List<String> genres = ['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi', 'Romance'];
+  List<String> selectedGenres = [];
+  File? _profileImage;
+  String? currentProfileImageUrl;
   User? user = FirebaseAuth.instance.currentUser;
 
   @override
   void initState() {
     super.initState();
-    nameController.text = user?.displayName ?? '';
+    _loadUserData(); // Firestore에서 기존 사용자 데이터 불러오기
   }
 
-  @override
-  void dispose() {
-    nameController.dispose();
-    super.dispose();
+  Future<void> _loadUserData() async {
+    DocumentSnapshot userData = await FirebaseFirestore.instance.collection('users').doc(user!.uid).get();
+    setState(() {
+      nameController.text = userData['name'];
+      currentProfileImageUrl = userData['profileImageUrl'];
+      selectedGenres = List<String>.from(userData['favoriteGenres']);
+    });
+  }
+
+  // 프로필 이미지 선택
+  Future<void> _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _profileImage = File(pickedImage.path);
+      });
+    }
+  }
+
+  // Firebase Storage에 프로필 이미지 업로드
+  Future<String?> _uploadProfileImage(String uid) async {
+    if (_profileImage != null) {
+      final storageRef = FirebaseStorage.instance.ref().child('profile_images/$uid.jpg');
+      await storageRef.putFile(_profileImage!);
+      return await storageRef.getDownloadURL();
+    }
+    return currentProfileImageUrl;  // 이미지가 변경되지 않았으면 기존 URL 반환
+  }
+
+  // Firestore에 사용자 정보 업데이트
+  Future<void> _updateUserProfile() async {
+    try {
+      String? profileImageUrl = await _uploadProfileImage(user!.uid);
+
+      await FirebaseFirestore.instance.collection('users').doc(user!.uid).update({
+        'name': nameController.text,
+        'profileImageUrl': profileImageUrl,
+        'favoriteGenres': selectedGenres,
+      });
+
+      await user?.updateDisplayName(nameController.text);  // Firebase Auth 사용자 이름 업데이트
+      await user?.reload();
+      setState(() {
+        user = FirebaseAuth.instance.currentUser;
+      });
+
+      Navigator.pop(context);
+    } catch (e) {
+      print('Error updating profile: $e');
+    }
   }
 
   @override
@@ -80,39 +163,60 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(labelText: 'Display Name'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  print('Attempting to update display name to: ${nameController.text}');
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // 프로필 사진 업로드 버튼
+              GestureDetector(
+                onTap: _pickImage,
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: _profileImage != null
+                      ? FileImage(_profileImage!)
+                      : currentProfileImageUrl != null
+                      ? NetworkImage(currentProfileImageUrl!) as ImageProvider
+                      : AssetImage('assets/default_profile.png'),
+                ),
+              ),
+              SizedBox(height: 20),
 
-                  // 사용자 이름 업데이트
-                  await user?.updateDisplayName(nameController.text);
-                  print('Display name update successful');
+              // 이름 입력 필드
+              TextField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: 'Display Name'),
+              ),
+              SizedBox(height: 20),
 
-                  // 사용자 정보 다시 불러오기
-                  await user?.reload();
-                  setState(() {
-                    user = FirebaseAuth.instance.currentUser;
-                  });
+              // 좋아하는 영화 장르 선택
+              Text('Select Your Favorite Movie Genres'),
+              Wrap(
+                spacing: 8.0,
+                children: genres.map((genre) {
+                  bool isSelected = selectedGenres.contains(genre);
+                  return ChoiceChip(
+                    label: Text(genre),
+                    selected: isSelected,
+                    onSelected: (selected) {
+                      setState(() {
+                        if (selected) {
+                          selectedGenres.add(genre);
+                        } else {
+                          selectedGenres.remove(genre);
+                        }
+                      });
+                    },
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 20),
 
-                  print('User info reloaded');
-                  print('Final Display Name: ${user?.displayName}');
-
-                  Navigator.pop(context);
-                } catch (e) {
-                  print('Error occurred: $e');
-                }
-              },
-              child: Text('Update Name'),
-            ),
-          ],
+              // 업데이트 버튼
+              ElevatedButton(
+                onPressed: _updateUserProfile,
+                child: Text('Update Profile'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignUpScreen extends StatefulWidget {
   @override
@@ -9,7 +14,31 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController majorController = TextEditingController();
+  final List<String> selectedGenres = [];
+  File? _profileImage;
+  final List<String> genres = ['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi', 'Romance'];
   bool isLoading = false;
+
+  Future<void> _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _profileImage = File(pickedImage.path);
+      });
+    }
+  }
+
+  Future<String?> _uploadProfileImage(String uid) async {
+    if (_profileImage != null) {
+      final storageRef = FirebaseStorage.instance.ref().child('profile_images/$uid.jpg');
+      await storageRef.putFile(_profileImage!);
+      return await storageRef.getDownloadURL();
+    }
+    return null;
+  }
+
 
   void _signUp() async {
     setState(() {
@@ -20,6 +49,18 @@ class _SignUpScreenState extends State<SignUpScreen> {
         email: emailController.text.trim(),
         password: passwordController.text.trim(),
       );
+
+      String uid = userCredential.user!.uid;
+      String? profileImageUrl = await _uploadProfileImage(uid);
+
+      await FirebaseFirestore.instance.collection('users').doc(uid).set({
+        'email': emailController.text.trim(),
+        'name': nameController.text.trim(),
+        'major': majorController.text.trim(),
+        'profileImageUrl': profileImageUrl,
+        'favoriteGenres': selectedGenres,
+      });
+
       Navigator.pushReplacementNamed(context, '/'); // 회원가입 후 홈 화면으로 이동
     } on FirebaseAuthException catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -36,29 +77,91 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sign Up'),
+        title: Text('Create Account Page'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            if (isLoading)
-              CircularProgressIndicator(),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: isLoading ? null : _signUp,  // 로딩 중에는 버튼 비활성화
-              child: Text('Sign Up'),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: Text(
+                  'Afterscene Logo (Letter)',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+              GestureDetector(
+                onTap: _pickImage,
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: _profileImage != null ? FileImage(_profileImage!) : null,
+                  child: _profileImage == null
+                    ? Icon(Icons.add_a_photo, size: 50)
+                    :null,
+                ),
+              ),
+
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+
+              TextField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: 'Enter your name'),
+              ),
+
+              // 전공 입력 필드
+              TextField(
+                controller: majorController,
+                decoration: InputDecoration(labelText: 'Enter your major'),
+              ),
+
+              SizedBox(height: 20),
+
+              // 영화 장르 선택
+              Text('What kind of Movie do you like..?'),
+              SizedBox(height: 10),
+
+              Wrap(
+                spacing: 8.0,
+                children: genres.map((genre) {
+                  bool isSelected = selectedGenres.contains(genre);
+                  return ChoiceChip(label: Text(genre),
+                      selected: isSelected,
+                      onSelected: (selected) {
+                    setState(() {
+                      if (selected) {
+                        selectedGenres.add(genre);
+                      } else {
+                        selectedGenres.remove(genre);
+                      }
+                    });
+                      },
+                  );
+                }).toList(),
+              ),
+
+              SizedBox(height: 20),
+
+              if (isLoading)
+                CircularProgressIndicator(),
+
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: isLoading ? null : _signUp,  // 로딩 중에는 버튼 비활성화
+                child: Text('Sign Up'),
+
+              ),
+            ],
+          ),
         ),
       ),
     );
